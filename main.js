ce77e09436263ec12958163c09d867d579e1ff4d
Saaniah Blankenberg <saaniahblankeberg@gmail.com>
2020-08-24 14:09:44 +0200

    Added scroll functions and allignments

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(document).scroll(function() {
+        var $nav = $(".top-nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+// $(document).ready(function() {
+//     $('.menu-toggler').on("click", function() {
+//         $(this).toggleClass("open");
+//         $(".top-nav").toggleClass("open");
+
+//     })
+// });
